f6c7921cfd93f1ee53dda9a5b0e9dbe59a3229e2
Stanislav Karpov <stkrp@yandex.ru>
2015-03-28 16:50:34 +0300

    Added default param as variable.

--- dict_get_default.py
+++ dict_get_default.py
@@ -3,23 +3,34 @@
 """
 
 dct = {1: 100, 2: 200, 3: 300, 4: 400, 5: 500, 6: 600, 7: 700, 8: 800, 9: 900, 10: [1, 2, 3, 4, 5]}
+default = {5: 1, 55: 2}
 
-a = dct.get(1)
+# Создание объекта значения по умолчанию в вызове get
 tmp = []
 for i in (11, 12):
     tmp.append(dct.get(i, {9: 1, 99: 2, 999: 3}))
 b, c = tmp
 
+# Использование заранее инициализированной переменной в качестве значения по умолчанию в вызове get
+tmp = []
+for i in (11, 12):
+    tmp.append(dct.get(i, default))
+d, e = tmp
+
 print(
-    'a', a,
-    '\nb', b,
-    '\nc', c
+    'b: {}'.format(b), 'c: {}'.format(c), 'd: {}'.format(d), 'e: {}'.format(e),
+    'default: {}'.format(default),
+    sep='\n'
 )
 
 b[9] = 10
 c[99] = 100
+d[5] = -1
+d[55] = -2
+d[555] = -3
 
 print(
-    'b', b,
-    '\nc', c
+    'upd b: {}'.format(b), 'upd c: {}'.format(c), 'upd d: {}'.format(d), 'upd e: {}'.format(e),
+    'default: {}'.format(default),
+    sep='\n'
 )
